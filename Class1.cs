30605066cb4a225f5f2e1488928fc13eb9923f36
Oleksandr Krasnoporov <oKrasnoporov@cofcointernational.com>
2018-07-16 11:41:04 +0300

    protopype

---
+++ Class1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Runtime.InteropServices;
+
+
+[assembly: SecurityPermissionAttribute(SecurityAction.RequestMinimum, UnmanagedCode = true)]
+[assembly: PermissionSetAttribute(SecurityAction.RequestMinimum, Name = "FullTrust")]
+
+namespace Imperson
+{
+    public class WindowsIdentityEx : IDisposable, IIdentity
+    {
+        #region Unmanaged Code
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool LogonUser(String lpszUsername, String lpszDomain, String lpszPassword,
+        int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+        [DllImport("kernel32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        private unsafe static extern int FormatMessage(int dwFlags, ref IntPtr lpSource,
+        int dwMessageId, int dwLanguageId, ref String lpBuffer, int nSize, IntPtr* Arguments);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public extern static bool CloseHandle(IntPtr handle);
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool DuplicateToken(IntPtr ExistingTokenHandle,
+        int SECURITY_IMPERSONATION_LEVEL, ref IntPtr DuplicateTokenHandle);
+
+        public unsafe static string GetErrorMessage(int errorCode)
+        {
+            int FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x00000100;
+            int FORMAT_MESSAGE_IGNORE_INSERTS = 0x00000200;
+            int FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000;
+            int messageSize = 255;
+            String lpMsgBuf = "";
+            int dwFlags = FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
+            IntPtr ptrlpSource = IntPtr.Zero;
+            IntPtr prtArguments = IntPtr.Zero;
+            int retVal = FormatMessage(dwFlags, ref ptrlpSource, errorCode, 0, ref lpMsgBuf, messageSize, &prtArguments);
+            if (0 == retVal)
+            {
+                throw new Exception("Failed to format message for error code " + errorCode + ". ");
+            }
+            return lpMsgBuf;
+        }
+        #endregion
+        private const int LOGON32_PROVIDER_DEFAULT = 0;
+        private const int LOGON32_LOGON_INTERACTIVE = 2;
+        private const int SecurityImpersonation = 2;
+
+        private bool _disposed = false;
+        private IntPtr _tokenHandle = new IntPtr(0);
+        private IntPtr _dupeTokenHandle = new IntPtr(0);
+        private string _userName = string.Empty;
+        private string _domainName = string.Empty;
+        private string _password = string.Empty;
+        private WindowsIdentity _windowsIdentity = null;
+        private void Init()
+        {
+            _tokenHandle = IntPtr.Zero;
+            _dupeTokenHandle = IntPtr.Zero;
+        }
+        public WindowsIdentityEx(string userName, string domainName, string password)
+        {
+            this.Init();
+            bool returnValue = LogonUser(userName, domainName, password, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, ref _tokenHandle);
+            if (false == returnValue)
+            {
+                int ret = Marshal.GetLastWin32Error();
+                int errorCode = 0x5; //ERROR_ACCESS_DENIED
+                throw new System.ComponentModel.Win32Exception(errorCode, string.Format("\nError: [{0}] {1}\n", ret, GetErrorMessage(ret)));
+            }
+            bool retVal = DuplicateToken(_tokenHandle, SecurityImpersonation, ref _dupeTokenHandle);
+            if (false == retVal)
+            {
+                CloseHandle(_tokenHandle);
+                throw new Exception("Unable to duplicate token");
+            }
+            _windowsIdentity = new WindowsIdentity(_dupeTokenHandle);
+        }
+        public WindowsIdentityEx(IntPtr userToken)
+        {
+            this.Init();
+            _windowsIdentity = new WindowsIdentity(userToken);
+        }
+        public WindowsIdentityEx(string sUserPrincipalName)
+        {
+            this.Init();
+            _windowsIdentity = new WindowsIdentity(sUserPrincipalName);
+        }
+        public WindowsIdentityEx(IntPtr userToken, string type)
+        {
+            this.Init();
+            _windowsIdentity = new WindowsIdentity(type);
+        }
+        public WindowsIdentityEx(string sUserPrincipalName, string type)
+        {
+            this.Init();
+            _windowsIdentity = new WindowsIdentity(sUserPrincipalName, type);
+        }
+        public WindowsIdentityEx(IntPtr userToken, string type, WindowsAccountType acctType)
+        {
+            this.Init();
+            _windowsIdentity = new WindowsIdentity(userToken, type, acctType);
+        }
+        public WindowsIdentityEx(IntPtr userToken, string type, WindowsAccountType acctType, bool isAuthenticated)
+        {
+            this.Init();
+            _windowsIdentity = new WindowsIdentity(userToken, type, acctType, isAuthenticated);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing && _windowsIdentity != null)
+                {
+                    _windowsIdentity.Dispose();
+                }
+                if (_tokenHandle != IntPtr.Zero)
+                {
+                    CloseHandle(_tokenHandle);
+                    _tokenHandle = IntPtr.Zero;
+                }
+                if (_dupeTokenHandle != IntPtr.Zero)
+                {
+                    CloseHandle(_dupeTokenHandle);
+                    _dupeTokenHandle = IntPtr.Zero;
+                }
+            }
+            _disposed = true;
+        }
+        ~WindowsIdentityEx()
+        {
+            Dispose(false);
+        }
+        string IIdentity.AuthenticationType
+        {
+            get { return _windowsIdentity.AuthenticationType; }
+        }
+        bool IIdentity.IsAuthenticated
+        {
+            get { return _windowsIdentity.IsAuthenticated; }
+        }
+        string IIdentity.Name
+        {
+            get { return _windowsIdentity.Name; }
+        }
+        public TokenImpersonationLevel ImpersonationLevel
+        {
+            get { return _windowsIdentity.ImpersonationLevel; }
+        }
+        public virtual bool IsAnonymous
+        {
+            get { return _windowsIdentity.IsAnonymous; }
+        }
+        public virtual bool IsGuest
+        {
+            get { return _windowsIdentity.IsGuest; }
+        }
+        public virtual bool IsSystem
+        {
+            get { return _windowsIdentity.IsSystem; }
+        }
+        public SecurityIdentifier Owner
+        {
+            get { return _windowsIdentity.Owner; }
+        }
+        public virtual IntPtr Token
+        {
+            get { return _windowsIdentity.Token; }
+        }
+        public SecurityIdentifier User
+        {
+            get { return _windowsIdentity.User; }
+        }
+        public WindowsIdentity WindowsIdentity
+        {
+            get { return _windowsIdentity; }
+        }
+        public virtual WindowsImpersonationContext Impersonate()
+        {
+            return _windowsIdentity.Impersonate();
+        }
+        public static WindowsImpersonationContext Impersonate(IntPtr userToken)
+        {
+            return WindowsIdentity.Impersonate(userToken);
+        }
+        public static WindowsIdentity GetAnonymous()
+        {
+            return WindowsIdentity.GetAnonymous();
+        }
+        public static WindowsIdentity GetCurrent()
+        {
+            return WindowsIdentity.GetCurrent();
+        }
+        public static WindowsIdentity GetCurrent(bool ifImpersonating)
+        {
+            return WindowsIdentity.GetCurrent(ifImpersonating);
+        }
+        public static WindowsIdentity GetCurrent(TokenAccessLevels desiredAccess)
+        {
+            return WindowsIdentity.GetCurrent(desiredAccess);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CacheCleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //clear cache
+            try
+            {
+                //string dir1 = "c:\\fff";
+                //listBox1.Items.Add("Deleting " + dir1);
+                //Directory.Delete(dir1, true); //удаление папки, и всего, что внутри
+                //listBox1.Items.Add("Deleted");
+                //listBox1.Items.Add("Cache files have been successfully deleted");
+
+                listBox2.Items.Clear();
+
+                string folderpath = "\\\\" + textBox1.Text + "\\c$\\users";
+                listBox1.Items.Add("Entering to: " + folderpath);
+
+                label5.Enabled = true;
+                listBox2.Enabled = true;
+                
+
+                DirectoryInfo dir = new DirectoryInfo(folderpath);
+                foreach (DirectoryInfo folder in dir.GetDirectories())
+                {
+                    if (folder.Exists)
+                    {
+                        listBox2.Items.Add(folder.Name);
+                    }
+                }
+
+                listBox1.Items.Add("Get profile list from: " + folderpath);
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("ERROR - " + ex);
+                //MessageBox.Show("There is no folder to delete or no access", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand); 
+            }
+        }
+
+        private void listBox2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string selectedItem = listBox2.SelectedItem.ToString();
+                string cachepath = "\\\\" + textBox1.Text + "\\c$\\users\\" + selectedItem + "\\AppData\\Local\\1C\\1cv8\\";
+                listBox1.Items.Add("Deleting cahe in directory: " + cachepath);
+                DirectoryInfo dir = new DirectoryInfo(cachepath);
+                listBox2.Items.Clear();
+                foreach (DirectoryInfo folder in dir.GetDirectories())
+                {
+                    if (folder.Exists)
+                    {
+
+                        if (folder.Name.Length > 20)
+                        {
+                            listBox2.Items.Add("deleting " + folder.Name);
+                            folder.Delete(true);
+                        }
+
+                    }
+                }
+
+                string cachepath2 = "\\\\" + textBox1.Text + "\\c$\\users\\" + selectedItem + "\\AppData\\Roaming\\1C\\1cv8\\";
+                listBox1.Items.Add("Deleting cahe in directory: " + cachepath2);
+                DirectoryInfo dir2 = new DirectoryInfo(cachepath2);
+                listBox2.Items.Clear();
+                foreach (DirectoryInfo folder in dir2.GetDirectories())
+                {
+                    if (folder.Exists)
+                    {
+
+                        if (folder.Name.Length > 20)
+                        {
+                            listBox2.Items.Add("deleting" + folder.Name);
+                            folder.Delete(true);
+                        }
+
+                    }
+                }
+                listBox1.Items.Add("Cache successfully deleted");
+            }
+            catch(Exception ex)
+            {
+                listBox1.Items.Add("ERROR - " + ex);
+                //MessageBox.Show("There is no folder to delete or no access", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand); 
+            };
+
+        }
+
+        private void listBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            string strTip = "";
+
+            //Get the item
+            int nIdx = listBox1.IndexFromPoint(e.Location);
+            if ((nIdx >= 0) && (nIdx < listBox1.Items.Count))
+                strTip = listBox1.Items[nIdx].ToString();
+
+            toolTip1.SetToolTip(listBox1, strTip);
+
+        }
+    }
+}
