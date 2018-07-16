ce94aced6e1fc5d8f43252da6c65263b38bacde0
Oleksandr Krasnoporov <oKrasnoporov@cofcointernational.com>
2018-07-16 11:44:15 +0300

    protopype2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -68,7 +68,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(274, 20);
             this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "adm_okrasnoporov1";
+            this.textBox2.Text = "";
             // 
             // label1
             // 
